6ee246715c0f2fda2baec897d4f40e5e81bc763d
James Naadjie <james.naadjie@thinkherrmann.com>
2022-10-30 10:50:03 -0400

    change firebase default public path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC0PyngA6tZhuNunlWNIetFnr1WGJlHXU4",
+    authDomain: "zado-48f38.firebaseapp.com",
+    projectId: "zado-48f38",
+    storageBucket: "zado-48f38.appspot.com",
+    messagingSenderId: "883365761632",
+    appId: "1:883365761632:web:502ce38e285716f7d72905",
+    measurementId: "G-V4457GYJ0G"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
   return (
     <div className="App">
       <header className="App-header">
