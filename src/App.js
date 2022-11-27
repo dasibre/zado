4bb7abb93b800fa363e2b162f2d118abce2d3248
James Naadjie <james.naadjie@thinkherrmann.com>
2022-11-27 14:07:21 -0500

    Added Material UI, Survey JS to setup basic ui and survey

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 import { initializeApp } from 'firebase/app';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { StylesManager,Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
+import 'survey-core/modern.min.css'
+
 
 function App() {
+  const surveyJson = {
+    elements: [{
+      name: "FirstName",
+      title: "Enter your first name:",
+      type: "text"
+    }, {
+      name: "LastName",
+      title: "Enter your last name:",
+      type: "text"
+    }]
+  };
+
+  const survey = new Model(surveyJson);
 
   const firebaseConfig = {
     apiKey: "AIzaSyC0PyngA6tZhuNunlWNIetFnr1WGJlHXU4",
@@ -14,26 +33,30 @@ function App() {
     measurementId: "G-V4457GYJ0G"
   };
 
-  // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  StylesManager.applyTheme("modern");
+
+  // Initialize Firebase; const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <CssBaseline />
+     <Container maxWidth="lg">
+       <Box sx={{color: 'black', width: '100%', height: '20vh', boxShadow: 1}}>
+         <div>
+         <Typography variant="h1" gutterBottom>
+          Zado
+          <Typography variant="subtitle2" gutterBottom>
+            Let us help you choose the best place for you and your family to live.
+          </Typography>
+         </Typography>
+         </div>
+       </Box>
+       <Box sx={{ bgcolor: 'info.main', width: '100%', height: '100vh', boxShadow: 3}}>
+        <Survey model={survey} />;
+       </Box>
+     </Container>
+    </>
+  )
 }
 
 export default App;
