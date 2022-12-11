40a8849a2204c25bee922b62035c3168e704e0f9
Eric Ofori <100205237+eric-ofori@users.noreply.github.com>
2022-12-11 22:34:13 +0000

    FirstNineQestions
    beginning

--- src/data/survey-questions.js
+++ src/data/survey-questions.js
@@ -10,17 +10,22 @@ const questions = {
   },
   {
     name: "commute",
-    title: "Will you be commutting to work",
-    type: "text",
-    visibleIf: "{employed} = yes"
+    title: "Will you be commuting to work?",
+    type: "radiogroup",
+    choices: [
+      "Yes", "No", "Sometimes"
+    ],
+    visibleIf: "{employed} = yes",
+    isRequired: true
   },
   {
     name: "school-children",
     title: "Do you have school age children?",
     type: "radiogroup",
     choices: [
-      "yes", "no"
+      "Yes", "No", "Not Right Now"
     ]
+
   },
   {
     name: "preferred-town",
@@ -38,11 +43,32 @@ const questions = {
     ]
   },
   {
+    name: "social-facilities",
+    title: "These towns have a lot of social facilities that you like.",
+    type: "radiogroup",
+    choices: [
+      "Yes", "No", "Indifferent"
+    ],
+    isRequired: true,
+  },
+  {
+    name: "education-profile",
+    title: "Would you like to include the education profile of the town residents in your analysis?",
+    type: "radiogroup",
+    choices: [
+      "I prefer towns with the highest levels of education",
+      "I prefer towns with average levels of education",
+      "Indifferent",
+      "No"
+    ],
+    isRequired: true,
+  },
+  {
     name: "race-composition",
     title: "Will you like to include the racial composition in your analysis?",
     type: "radiogroup",
     choices: [
-      "yes", "no"
+      "Yes", "No"
     ],
     isRequired: true,
   },
@@ -54,7 +80,18 @@ const questions = {
       "diverse", "black", "white", "non-white", "dont-care"
     ],
     visibleIf: "{race-composition} = yes"
-  }
+  }, 
+  {
+    name: "recreational-facilities",
+    title: "Generally, towns with places of worship, parks, and recreational facilities have a much more vibrant social life than towns without",
+    type: "radiogroup",
+    choices: [
+      "Yes, I prefer a town with a lot of civic organizations, Parks, Recreational facilities",
+      "No, I prefer a town with less civic organizations, Parks, Recreational facilities",
+      "I am indifferent"
+    ],
+    isRequired: true,
+  },
 ]
 };
 
