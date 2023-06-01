2c1b6b700997b4ec56cf7d793c936711ae397e20
James Naadjie <james.naadjie@thinkherrmann.com>
2023-05-31 23:15:42 -0400

    Implemented getcities to fetch each city given a city id

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import NavBar from './components/NavBar';
 import SurveyContent from './components/SurveyContent';
 import { StylesManager, Model } from 'survey-core';
 import questions from './data/survey-questions';
-// import { getCities } from './services/getData';
+import { getCities } from './services/getData';
 import "./App.css"
 
 StylesManager.applyTheme("modern");
 
 function App() {
-  // const [cities, setCities] = useState([]);
+  const [cities, setCities] = useState([]);
   const [survey] = useState(new Model(questions));
   const [pageChangeCounter, setPageChangeCounter] = React.useState(0);
   
@@ -23,27 +23,15 @@ function App() {
   });
   const [started, start] = useState(false);
 
-  const surveyComplete = (sender) => {
-    console.log(sender.data);
+  const surveyComplete = async (sender) => {
+    const cityData = sender.data['preferred-town'].map(id => getCities(id).then(data => data));
+    await Promise.all(cityData).then(function(results) {
+      setCities(results);
+    })
   }
 
   survey.onComplete.add(surveyComplete)
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const data = await getCities();
-  //     const choices = data.map((city) => {
-  //       return {
-  //         valueName: city.one_code,
-  //         value: city.city_name,
-  //       }
-  //     })
-  //     console.log(choices.length)
-  //     setCities(choices.slice(0,1000));
-  //   }
-  //   fetchData().catch(console.error);
-  // },[]);
-  
 
   return (
     <>
@@ -52,6 +40,9 @@ function App() {
         <Stack direction="row" spacing={2} justifyContent="center">
           <SideBar survey={survey} started={started} onClick={() => start(true)}/>
           <SurveyContent survey={survey} started={started}/>
+          <Box p={2} flex={2}>
+            {cities.map(city => <div>{city.city_name}</div>)}
+          </Box>
         </Stack>
     </Box>
     </>
--- src/services/getData.js
+++ src/services/getData.js
@@ -1,9 +1,9 @@
 import { db, dbref } from '../firebase';
 import {child, get} from "firebase/database";
 
-export const getCities = async () => {
+export const getCities = async (id) => {
     const citiesRef = dbref(db);
-    return get(child(citiesRef, 'cities')).then((snapshot) => {
+    return get(child(citiesRef, `cities/${id}`)).then((snapshot) => {
         if (snapshot.exists()) {
             const data = snapshot.val();
             return data
