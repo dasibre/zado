069abf7b52ed167a8af65fb2dacaf728320d744b
devwence <95927210+devwence@users.noreply.github.com>
2025-04-10 10:28:24 +0000

    updating cors

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,38 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const axios = require("axios");
-const cors = require("cors");
+
+const cors = require("cors")({ origin: true });
 
 // Use Firebase config to access API key
 // const GREAT_SCHOOLS_API_KEY = "";
 
 exports.fetchschooldata = onRequest((req, res) => {
+  cors(req, res, () => {
+    const { state, city } = req.query;
 
-  corsHandler(req, res, () => {
-  const { state, city } = req.query;
+    if (!state || !city) {
+      return res.status(400).send({ error: "State and city parameters are required." });
+    }
 
-  if (!state || !city) {
-    return res.status(400).send({ error: "State and city parameters are required." });
-  }
+    try {
+      const response =  axios.get(`https://gs-api.greatschools.org/v2/schools`, {
+        params: { state, city },
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": GREAT_SCHOOLS_API_KEY,
+        },
+      });
 
-  try {
-    const response =  axios.get(`https://gs-api.greatschools.org/v2/schools`, {
-      params: { state, city },
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": GREAT_SCHOOLS_API_KEY,
-      },
-    });
+      const schools = Array.isArray(response.data.schools) ? response.data.schools : [];
+      res.status(200).send({ schools });
+    } catch (error) {
+      logger.error("Error fetching school data:", error);
 
-    const schools = Array.isArray(response.data.schools) ? response.data.schools : [];
-    res.status(200).send({ schools });
-  } catch (error) {
-    logger.error("Error fetching school data:", error);
+      const status = error.response?.status || 500;
+      const message = error.response?.data || "Internal Server Error";
 
-    const status = error.response?.status || 500;
-    const message = error.response?.data || "Internal Server Error";
-
-    res.status(status).send({ error: message });
-  }
-});
+      res.status(status).send({ error: message });
+    }
+  });
 });
